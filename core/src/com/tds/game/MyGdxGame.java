a7cbb7ef836973d63b593082c8268496aa85fd0d
prostoblodi <dimatiktok.like8@gmail.com>
2024-07-07 21:02:23 +0300

    base

---
+++ core/src/com/tds/game/MyGdxGame.java
@@ -0,0 +1,27 @@
+package com.tds.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.assets.AssetManager;
+
+public class MyGdxGame extends Game {
+	private AssetManager assetManager;
+
+	@Override
+	public void create() {
+		// Получаем информацию о дисплее
+		Graphics.DisplayMode displayMode = Gdx.graphics.getDisplayMode();
+
+		// Устанавливаем полноэкранный режим с использованием текущей разрешения дисплея
+		Gdx.graphics.setFullscreenMode(displayMode);
+		assetManager = new AssetManager();
+		setScreen(new LoadingScreen(this, assetManager));
+	}
+
+	@Override
+	public void dispose() {
+		assetManager.dispose();
+		super.dispose();
+	}
+}
